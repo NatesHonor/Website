0de24e7d900a1acee4d61c31ba40ebcf437b2ec5
qNateYT <nathaniel.j.marcellus@gmail.com>
2023-07-28 14:31:40 -0400

    Created and Finalized Website

--- src/functions/ResumeContent.js
+++ src/functions/ResumeContent.js
@@ -1,65 +1,175 @@
 import React from 'react';
-import '../styles/ResumeContent.css'
+import '../styles/ResumeContent.css';
+
 const ResumeContent = () => (
   <div className="resume-content">
-    <div className="contact-info">
-      <h2>Nathaniel (Nate) Marcellus</h2>
-      <p>Marietta, GA 30064</p>
-      <p>nathaniel.j.marcellus@gmail.com</p>
-      <p>+1 678 748 7858</p>
-    </div>
+    <h2>Nathaniel Marcellus</h2>
     <h3>About Me</h3>
+    <p>Marietta, GA 30064</p>
+    <p>nathaniel.j.marcellus@gmail.com</p>
+    <p>+1 678 748 7858</p>
+    <h2>Nathaniel Marcellus</h2>
     <p>
-      My Name is Nate, I'm 16 turning 17,  I love coding, socializing with people, and helping anyone who needs it. In my honest opinion, I'm an opportunist and I love trying new things to see what can help me out now to better my future later. I've also worked as an insider for Marco's Pizza, and after the very short time I've worked there, every manager I had has nothing but good things to say about me.
+      <strong>About Me</strong><br />
+      Hello, my name is Nate, I am a 17-year-old developer who loves coding. Over the past seven years,
+      I have honed his skills in CSS, JavaScript, and other web development tools. I am skilled at creating
+      visually appealing websites and have a good understanding of web UX principles. Additionally, I started
+      working on web applications and reactive websites.
     </p>
-    <div className='authorized'>
-    <p>
-      <strong>Authorized to work in the US for any employer</strong>
-    </p>
-    </div>  
     <h3>Work Experience</h3>
+    <p>
+      <strong>Freelance Developer</strong><br />
+      Elemental Legacy<br />
+      June 2023 to Present<br />
+      As a freelance developer for this company they had me doing tasks such as Website Design &
+      Development, Javascript Application Development, Java Plugin Development. In the short time i've been
+      working for this company I got promoted from the junior position to the developer position in a matter
+      of days due to my sheer dedication, work force, and will power.
+    </p>
+    <p>
+      <strong>Team Lead/Software Engineer</strong><br />
+      Abyssal Hosting LLC. (Formally Papyrus Nodes)<br />
+      February 2022 to Present<br />
+      As Team Lead/Software Engineer it was my job to develop the software needed to host all the necessary
+      products that the executives needed. WIthin a week I had the first prototype done and ready to deploy
+      and surprisingly it exceeded what was required by the executives. Through my sheer dedication and the
+      fact that I've been working with the executives for almost 4 years, I got promoted to an Executive position
+      but with everything being deployed and barely any issues arising besides some routine maintenance or
+      software upgrades there's not a lot for me to do.
+    </p>
     <p>
       <strong>Insider</strong><br />
       Marco's Pizza - Marietta, GA<br />
-      August 2022 to Present<br />
-      I work as an insider, and although I've only worked there for about two months, I've given my managers and team workers nothing but good things to say about me.
+      August 2022 to November 2023
     </p>
     <p>
-      <strong>Insider</strong><br />
-      August 2022 to November 2022<br />
-      As an insider, my job is to act as an authoritative figure for new members if the manager is not around, answer phones, provide customer service, handle cash, and assist with hospitality.
+      <strong>Team Member</strong><br />
+      Wendy's - Marietta, GA<br />
+      January 2023 to April 2023
     </p>
+    <p>
+      <strong>Sr. Developer</strong><br />
+      Papyrus Network<br />
+      June 2020 to September 2022<br />
+      As a senior developer, I was in charge of my development team. While leading my team, my team and
+      I were handling any task thrown at us in 1-2 days or less. Through my sheer dedication and work force
+      I was able to help the Executives get whatever they needed to be pushed out well before their deadline
+      and fixed any problems that arrose with the utmost diligance and speed.
+    </p>
+    <p>
+      <strong>Software Engineer</strong><br />
+      Papyrus Nodes (Now Abyssal Hosting)<br />
+      June 2020 to February 2022<br />
+      Papyrus Nodes was a small start-up company that wanted to push hosting and product-related
+      development to people in need or with a lack of staff. When I was brought onto the team it wasn't a
+      skeleton crew, but with the lack of activity, motivation, and attendance, it was as if it was one. I frequently
+      butted heads with my Manager at the time as he wanted to keep lazy people who obviously don't know
+      what they were doing and how to do their job on our team. I ended up going around him to talk to a
+      higher-up who then said they would sort it out and ended up firing anyone who wasn't contributing to
+      the team. From then on I worked hard to push software, plugins, and updates as needed which caused
+      me to become liked by the higher-ups. I then was promoted to Team Lead/ Sr.Software engineer after
+      two years following the rename to Abyssal Hosting.
+    </p>
+    <p>
+  On the side to help support my development as well as to make a steady source of income, I've
+  worked a few fast food jobs. This includes Marco's Pizza, Wendy's, And Burger King.
+  <br></br>
+  <br></br>
+  I worked at Marcos pizza for a good 3-4 months if not more, while working there I got to feel the sense
+  of being a part of another family who I'm not related to. While working there it taught me discipline
+  and hospitality causing customers and management alike to always enjoy working with me. Had it not
+  been for the low pay (started me at $7.25) I would have loved to keep working there.
+  <br></br>
+  <br></br>
+  While working at Wendy's it truly showed me the stresses of working in a high pace environment and
+  how important it is that everyone does their job. I honestly loved working there and I loved the pay, I
+  just couldn't get enough hours and a coworker was trying to push me to commit crimes for him. With
+  that being said and I alerted a police officer I was advised to look for another job.
+  <br></br>
+  <br></br>
+  Burger King was an interesting experience for me, yes it was fast-paced but we never really got in
+  trouble if we didn't rush and I did meet some pretty nice people, I am currently employed there but
+  I'm not getting enough hours so I'm trying to find new work. I have discussed this with management
+  several times but they refuse to give me enough hours to even make more than $100 every other
+  week even though shifts are understaffed.
+</p>
+<p>
+  <strong>Development/Coding Wise</strong><br />
+  Some projects I've worked on include coding games, launchers, mods,
+  plugins, and much more. I even coded some plugins for the GitHub and Spigot platforms, giving me
+  versatility and adaptability in different development environments. I am highly committed to keeping
+  up with the latest industry trends to keep my programming skills up to date. I enjoy challenges and
+  approach each project with determination and innovative thinking.
+  <br></br>
+  <br></br>
+  As a devoted programmer, I strive to bring my specialized knowledge and drive to any development
+  team, thereby making a big impact on their systems, projects, and way of thinking. In addition to my
+  web development knowledge, I've broadened my programming knowledge to include a variety of
+  other programming languages. With a solid foundation in Java, C#, C, SQL, and some other coding
+  languages. I have worked on multiple projects (mostly private) to help test my abilities in action.
+  These projects gave me hands-on experience in developing a wide variety of solutions, from desktop
+  applications to database management systems. This hands-on approach not only deepened my
+  knowledge of programming languages but also allowed me to explore the simplicities and complexities
+  of each language. Constantly taking on new programming tasks has made me more adaptable and
+  versatile in different development environments.
+  <br></br>
+  <br></br>
+  Seamlessly switching between languages and frameworks allows us to tackle complex problems
+  and develop innovative solutions. I am passionate about advancing the industry and staying at the
+  forefront of new technologies. Through continuous learning and a keen eye for the latest trends, I
+  always attempt to make sure my coding skills are always up-to-date and relevant.
+  Proactivity allows me to deliver innovative solutions that meet the changing needs of our customers
+  and end-users. Driven by my passion for programming and an insatiable thirst for knowledge, I
+  approach every project with unwavering determination and creative thinking. I enjoy challenges and
+  embrace them as opportunities to learn and grow. As a dedicated programmer, I look forward to
+  bringing my technical expertise and unwavering drive to any development team to make a big impact
+  on projects and push the boundaries of what's possible.
+</p>
     <h3>Education</h3>
     <p>
-      <strong>Junior High School Education</strong><br />
+      <strong>High school diploma</strong><br />
       Marietta High School - Marietta, GA<br />
       August 2020 to Present
     </p>
     <h3>Skills</h3>
     <ul>
-    <li>Communication skills</li>
-    <li>Math</li>
-    <li>Basic Math</li>
-    <li>Basic Math</li>
-    <li>Organizational Skills</li>
-    <li>Typing</li>
-    <li>Computer Operation</li>
-    <li>Curriculum Development</li>
-    <li>Python</li>
-    <li>JavaScript</li>
-    <li>JavaScript</li>
-    <li>Web development</li>
-    <li>React</li>
-    <li>MySQL</li>
-    <li>Node.js</li>
-    <li>CSS</li>
-    <li>Git</li>
-    <li>HTML5</li>
-    <li>Java</li>
-    <li>User Interface (UI)</li>
+      <li>Communication skills</li>
+      <li>Math</li>
+      <li>Basic Math</li>
+      <li>Organizational Skills</li>
+      <li>Typing</li>
+      <li>Computer Operation</li>
+      <li>Curriculum Development</li>
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>Web development</li>
+      <li>React</li>
+      <li>MySQL</li>
+      <li>Node.js</li>
+      <li>CSS</li>
+      <li>Git</li>
+      <li>HTML5</li>
+      <li>Java</li>
+      <li>User Interface (UI)</li>
+      <li>PHP</li>
+      <li>APIs</li>
+      <li>XML</li>
+      <li>GitHub</li>
+      <li>Microsoft SQL Server</li>
+      <li>AWS</li>
+      <li>REST</li>
+      <li>Test Cases</li>
+      <li>Docker</li>
+      <li>Bootstrap</li>
+      <li>JSON</li>
+      <li>Jira</li>
+      <li>MongoDB</li>
+      <li>Wordpress</li>
     </ul>
     <h3>Languages</h3>
     <p>English - Fluent</p>
+    <h3>Links</h3>
+    <p><a href="https://github.com/NatesHonor">https://github.com/NatesHonor</a></p>
     <h3>Assessments</h3>
     <p>
       <strong>Cooking skills: Basic food preparation — Proficient</strong><br />
@@ -89,7 +199,8 @@ const ResumeContent = () => (
     <p>
       <strong>Inside sales — Proficient</strong><br />
       June 2022<br />
-      Understanding and responding appropriately in sales scenarios, and performing common sales calculations
+      Understanding and responding appropriately in sales scenarios, and performing common sales
+      calculations
     </p>
     <p>
       <strong>Retail customer service — Proficient</strong><br />
@@ -107,8 +218,27 @@ const ResumeContent = () => (
       Responding to customer situations with sensitivity
     </p>
     <p>
-      Indeed Assessments provides skills tests that are not indicative of a license or certification, or continued development in any professional field.
+      Indeed Assessments provides skills tests that are not indicative of a license or certification, or continued
+      development in any professional field.
     </p>
+    <h3>Additional Information</h3>
+    <p>
+      I would like to highlight that I don't have a plethora of public resources available to showcase my skills.
+      The nature of the work I engage in restricts me from publicly sharing or uploading the source code or
+      details of most of my projects. However, I am more than willing to prove my abilities by undertaking a
+      predetermined task within a specified time limit to demonstrate my skills effectively.
+      Recently, I have been honing my skills in React, a powerful JavaScript library for building user
+      interfaces. To put my abilities to the test and explore the full potential of React, I took on the task
+      of creating a personal portfolio website. Although the website is still a work in progress and lacks
+      extensive content, I find it quite cool and impressive.
+      While my portfolio website may not have a significant amount of content at the moment, it serves as a
+      tangible demonstration of my capabilities in React. It can be utilized to showcase my skills to potential
+      employers or clients who are interested in evaluating my expertise in React development.
+    </p>
+
+    <div className='authorized'>
+      <p><strong>Authorized to work in the US for any employer</strong></p>
+    </div>
   </div>
 );
 
